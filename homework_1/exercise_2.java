cf47b059b06119d86147300345ec98e7f350d4fa
MetallMuff <107746199+MetallMuff@users.noreply.github.com>
2022-10-17 14:29:18 +1000

    homework_1

---
+++ homework_1/exercise_2.java
@@ -0,0 +1,15 @@
+// 27. Remove Element
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        if (nums.length == 0)
+            return 0;
+        int index = 0;
+        for (int x : nums) {
+            if (x != val) {
+                nums[index] = x;
+                index++;
+            }
+        }
+        return index;
+    }
+}
